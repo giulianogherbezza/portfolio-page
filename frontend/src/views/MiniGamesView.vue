8c8639cd086967bc67f858e49a2b6ef1861e54df
Giuliano Gherbezza <giuliano.gherbezza@students.bfh.ch>
2025-03-07 18:14:57 +0100

    Nur minigames mit https

--- frontend/src/views/MiniGamesView.vue
+++ frontend/src/views/MiniGamesView.vue
@@ -10,14 +10,7 @@
         <button @click="checkTriviaAnswer(answer)">{{ answer }}</button>
       </div>
       <p v-if="triviaFeedback">{{ triviaFeedback }}</p>
-    </div>
-
-    <!-- Number Facts -->
-    <div class="game-card">
-      <h2>Number Facts</h2>
-      <input v-model="numberInput" type="number" placeholder="Enter a number" />
-      <button @click="fetchNumberFact">Get Fact</button>
-      <p>{{ numberFact }}</p>
+      <button @click="fetchTriviaQuestion">Nächste Frage</button>
     </div>
 
     <!-- Joke Generator -->
@@ -25,14 +18,49 @@
       <h2>Joke Generator</h2>
       <p>{{ joke.setup }}</p>
       <p v-if="joke.punchline">{{ joke.punchline }}</p>
-      <button @click="fetchJoke">New Joke</button>
+      <button @click="fetchJoke">Neuer Witz</button>
+    </div>
+
+    <!-- Pokémon Guessing Game -->
+    <div class="game-card">
+      <h2>Pokémon Guessing Game</h2>
+      <img
+        v-if="pokemon.image"
+        :src="pokemon.image"
+        alt="Pokémon"
+        class="game-image"
+      />
+      <input v-model="pokemonGuess" placeholder="Welches Pokémon ist das?" />
+      <button @click="checkPokemonGuess">Guess</button>
+      <p>{{ pokemonFeedback }}</p>
+      <button @click="fetchPokemon">Neues Pokémon</button>
+    </div>
+
+    <!-- Country Flag Quiz -->
+    <div class="game-card">
+      <h2>Country Flag Quiz</h2>
+      <img
+        v-if="country.flag"
+        :src="country.flag"
+        alt="Country Flag"
+        class="game-image"
+      />
+      <div v-for="(option, index) in countryOptions" :key="index">
+        <button @click="checkCountryGuess(option)">{{ option }}</button>
+      </div>
+      <p>{{ countryFeedback }}</p>
+      <button @click="fetchCountry">Neues Land</button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted } from "vue";
 
+// Shuffle helper
+const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+
+//////////////////////////
 // Trivia Quiz
 const triviaQuestion = ref({});
 const triviaAnswers = ref([]);
@@ -47,24 +75,15 @@ const fetchTriviaQuestion = async () => {
     ...question.incorrect_answers,
     question.correct_answer,
   ]);
+  triviaFeedback.value = "";
 };
 
 const checkTriviaAnswer = (answer) => {
   triviaFeedback.value =
-    answer === triviaQuestion.value.correct_answer
-      ? "Correct!"
-      : "Wrong answer!";
+    answer === triviaQuestion.value.correct_answer ? "Richtig!" : "Falsch!";
 };
 
-// Number Facts
-const numberInput = ref("");
-const numberFact = ref("");
-
-const fetchNumberFact = async () => {
-  const res = await fetch(`http://numbersapi.com/${numberInput.value}`);
-  numberFact.value = await res.text();
-};
-
+//////////////////////////
 // Joke Generator
 const joke = ref({ setup: "", punchline: "" });
 
@@ -73,11 +92,73 @@ const fetchJoke = async () => {
   joke.value = await res.json();
 };
 
-// Shuffle helper
-const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+//////////////////////////
+// Pokémon Guessing Game
+const pokemon = ref({ name: "", image: "" });
+const pokemonGuess = ref("");
+const pokemonFeedback = ref("");
 
+const fetchPokemon = async () => {
+  const id = Math.floor(Math.random() * 151) + 1;
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const data = await res.json();
+  pokemon.value = {
+    name: data.name.toLowerCase(),
+    image: data.sprites.front_default,
+  };
+  pokemonFeedback.value = "";
+  pokemonGuess.value = "";
+};
+
+const checkPokemonGuess = () => {
+  if (pokemonGuess.value.toLowerCase() === pokemon.value.name) {
+    pokemonFeedback.value = "Richtig!";
+  } else {
+    pokemonFeedback.value = `Falsch! Es war ${pokemon.value.name}`;
+  }
+};
+
+//////////////////////////
+// Country Flag Quiz
+const countries = ref([]);
+const country = ref({ name: "", flag: "" });
+const countryOptions = ref([]);
+const countryFeedback = ref("");
+
+const fetchCountry = async () => {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const data = await res.json();
+  countries.value = data;
+  const selected = data[Math.floor(Math.random() * data.length)];
+  country.value = {
+    name: selected.name.common,
+    flag: selected.flags.svg,
+  };
+  countryOptions.value = shuffle([
+    selected.name.common,
+    ...shuffle(data.filter((c) => c.name.common !== selected.name.common))
+      .slice(0, 3)
+      .map((c) => c.name.common),
+  ]);
+  countryFeedback.value = "";
+};
+
+const checkCountryGuess = (guess) => {
+  if (guess === country.value.name) {
+    countryFeedback.value = "Richtig!";
+  } else {
+    countryFeedback.value = `Falsch! Es war ${country.value.name}`;
+  }
+};
+
+//////////////////////////
+
+//////////////////////////
+// Init
 onMounted(() => {
   fetchTriviaQuestion();
   fetchJoke();
+  fetchPokemon();
+  fetchCountry();
 });
 </script>
